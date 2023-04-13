a41f2cc7653c20d7a3d1485b0fc22c84e1122c5a
MAXBIT\v.zybailo <v.zybailo@jetmail.cc>
2023-04-13 17:49:44 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// burger menu
+let btn = document.querySelector('#burger-container')
+let burgMenu = document.querySelector('.burger__menu')
+let doc = document.querySelector('html')
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open')
+    burgMenu.classList.toggle('show')
+    if(btn.classList.contains('open')) {
+        doc.style.overflow = 'hidden'
+    } else {
+        doc.style.overflow = 'auto'
+    }
+})
